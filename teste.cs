23e8ba3dcb79045a177bedc77512124c2aee581b
lucasfera15 <65260218+lucasfera15@users.noreply.github.com>
2020-05-13 14:32:47 -0300

    Teste: Mosta o nome e a idade de pessoas presente em uma lista
    Mosta o nome e a idade de pessoas presente em uma lista,feito na unity em c# uheuheueh ;-; a função Update comentada é da unity

---
+++ teste.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Pessoas
+{
+    public string nome;
+    public int idade;
+}
+public class teste : MonoBehaviour
+{
+
+    public Pessoas[] peoples;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < peoples.Length; i++)
+        {
+            Debug.Log(peoples[i].nome);
+            Debug.Log(peoples[i].idade);
+
+            //Mesma coisa que "print" ;-;
+        }
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+    //    
+    //}
+}
